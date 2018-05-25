87a3d867f5ae5553164c0d213de83cfa3fc99134
Mohammad Ghifari Haekal <ghifarihaekal7@gmail.com>
2018-05-25 23:32:38 +0700

    tabbed contents added
    Biar user nggak susah ngescroll

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
 
+<!doctype html>
+
+@extends('layouts.navbar')
+
+@section('content')
+
 <html lang="en">
     <head>
         <meta charset="utf-8">
@@ -22,7 +28,7 @@
 	<div class="main-container">
 	<!-- image slider -->
 	<!-- <section>
-			<div class="container">				
+			<div class="container">
 				<div class="row">
 					<div class="col-sm-12">
 						<div class="image-slider slider-all-controls controls-inside">
@@ -38,46 +44,53 @@
 								</li>
 							</ul>
 						</div>
-						
+
 					</div>
 				</div>
-				
+
 			</div>
 	</section> -->
-	
+
 	<!-- countdown -->
-	<section class="cover fullscreen image-bg overlay">
-		<div class="background-image-holder">
-			<img alt="image" class="background-image" src="img/cover13.jpg">
-		</div>
-		<div class="container v-align-transform">
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="text-center">
-						<h3 class="uppercase mb40 mb-xs-24">Launching Soon</h3>
-						<div class="countdown mb40" data-date="2018/05/26"></div>
-					</div>
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 text-center">
-					<p>
-						We'll be launching our new site in the coming weeks. Hit the form below to get notified as we launch. Thanks for your interest!
-					</p>
-					<form class="halves form-newsletter" data-success="Thanks for your submission, we will be in touch shortly." data-error="Please fill all fields correctly.">
-						<input class="mb16 validate-required validate-email signup-email-field" type="text" placeholder="Email Address" name="email">
-						<button class="mb16" type="submit">Notify Me</button>
-						<span>*We won't share your email with third parties.</span>
-						<iframe srcdoc="<!-- Begin MailChimp Signup Form --><link href=&quot;https://cdn-images.mailchimp.com/embedcode/classic-081711.css&quot; rel=&quot;stylesheet&quot; type=&quot;text/css&quot;><style type=&quot;text/css&quot;>    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */</style><div id=&quot;mc_embed_signup&quot;><form action=&quot;//mrare.us8.list-manage.com/subscribe/post?u=77142ece814d3cff52058a51f&amp;id=94d040322a&quot; method=&quot;post&quot; id=&quot;mc-embedded-subscribe-form&quot; name=&quot;mc-embedded-subscribe-form&quot; class=&quot;validate&quot; target=&quot;_blank&quot; novalidate>    <div id=&quot;mc_embed_signup_scroll&quot;>    <h2>Subscribe to our mailing list</h2><div class=&quot;indicates-required&quot;><span class=&quot;asterisk&quot;>*</span> indicates required</div><div class=&quot;mc-field-group&quot;>    <label for=&quot;mce-EMAIL&quot;>Email Address  <span class=&quot;asterisk&quot;>*</span></label>    <input type=&quot;email&quot; value=&quot;&quot; name=&quot;EMAIL&quot; class=&quot;required email&quot; id=&quot;mce-EMAIL&quot;></div><div class=&quot;mc-field-group&quot;>    <label for=&quot;mce-FNAME&quot;>First Name </label>    <input type=&quot;text&quot; value=&quot;&quot; name=&quot;FNAME&quot; class=&quot;&quot; id=&quot;mce-FNAME&quot;></div><div class=&quot;mc-field-group&quot;>    <label for=&quot;mce-LNAME&quot;>Last Name </label>    <input type=&quot;text&quot; value=&quot;&quot; name=&quot;LNAME&quot; class=&quot;&quot; id=&quot;mce-LNAME&quot;></div>    <div id=&quot;mce-responses&quot; class=&quot;clear&quot;>        <div class=&quot;response&quot; id=&quot;mce-error-response&quot; style=&quot;display:none&quot;></div>        <div class=&quot;response&quot; id=&quot;mce-success-response&quot; style=&quot;display:none&quot;></div>    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->    <div style=&quot;position: absolute; left: -5000px;&quot;><input type=&quot;text&quot; name=&quot;b_77142ece814d3cff52058a51f_94d040322a&quot; tabindex=&quot;-1&quot; value=&quot;&quot;></div>    <div class=&quot;clear&quot;><input type=&quot;submit&quot; value=&quot;Subscribe&quot; name=&quot;subscribe&quot; id=&quot;mc-embedded-subscribe&quot; class=&quot;button&quot;></div>    </div></form></div><script type='text/javascript' src='https://s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->" class="mail-list-form">
-						</iframe>
-					</form>
-				</div>
-			</div>
-			
-		</div>		
-	</section>
-					
+  <section class="cover fullscreen image-bg overlay parallax">
+      <ul class="slides">
+          <li class="overlay image-bg bg-light">
+              <div class="background-image-holder">
+                  <img alt="image" class="background-image" src="img/home23.jpg" />
+              </div>
+              <div class="container v-align-transform">
+                  <div class="row">
+                      <div class="col-sm-10 col-sm-offset-1 text-center">
+                          <h1 class="mb40 mb-xs-16 large">IT Today 2018</h1>
+                      </div>
+                  </div>
+                  <!--end of row-->
+              </div>
+              <!--end of container-->
+          </li>
+          <li class="overlay image-bg">
+              <div class="background-image-holder">
+                  <img alt="image" class="background-image" src="img/home22.jpg" />
+              </div>
+              <div class="container v-align-transform">
+                  <div class="row">
+                      <div class="col-sm-offset-1 text-center col-sm-10">
+                          <h1 class="mb40 mb-xs-16 large">Foundry brings your content to life in stunning clarity</h1>
+                          <h6 class="uppercase mb16">A complete block-based solution</h6>
+                          <p class="lead mb40">
+                              Build beautiful, contemporary sites in just moments
+                              <br />with Foundry and Variant Page Builder.
+                          </p>
+                          <a class="btn btn-lg btn-white" href="#">Start Exploring</a>
+                      </div>
+                  </div>
+                  <!--end of row-->
+              </div>
+              <!--end of container-->
+          </li>
+      </ul>
+  </section>
+
 
 	<!-- Dokumentasi -->
 	<section class="bg-dark pb64">
@@ -91,7 +104,7 @@
 					</p>
 				</div>
 			</div>
-			
+
 			<div class="row">
 				<div class="col-sm-12">
 					<div class="lightbox-grid square-thumbs" data-gallery-title="Gallery">
@@ -154,10 +167,10 @@
 							</li>
 						</ul>
 					</div>
-					
+
 				</div>
 			</div>
-			
+
 		</div>
 
 		<div class="container">
@@ -186,15 +199,59 @@
 					</p>
 				</div>
 			</div>
-			
+
 		</div>
 	</section>
+  <!--Lebih praktis untuk user daripada ngescroll kepanjangan-->
+  <section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 col-sm-12 text-center">
+                <h4 class="uppercase mb80">Rangkaian Acara</h4>
+                <div class="tabbed-content button-tabs">
+                    <ul class="tabs">
+                        <li class="active">
+                          <a href="#appstoday">
+                            <div class="tab-title">
+                                <span>Apps Today</span>
+                            </div>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#hacktoday">
+                            <div class="tab-title">
+                                <span>Hack Today</span>
+                            </div>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#businessit">
+                            <div class="tab-title">
+                                <span>Business IT</span>
+                            </div>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#semnasit">
+                            <div class="tab-title">
+                                <span>Seminar IT Nasional</span>
+                            </div>
+                          </a>
+                        </li>
+                    </ul>
+                </div>
+                <!--end of button tabs-->
+            </div>
+        </div>
+        <!--end of row-->
+    </div>
+  </section>
 
 	<section class="image-bg overlay parallax pt180 pb180 pt-xs-80 pb-xs-80" id="appstoday">
 		<div class="background-image-holder">
 			<img alt="image" class="background-image" src="img/apps.png">
 		</div>
-		
+
 		<div class="container">
 			<div class="row">
 				<div class="col-md-5 col-sm-6 col-md-push-7 col-sm-push-6">
@@ -211,8 +268,8 @@
 		<div class="background-image-holder">
 			<img alt="image" class="background-image" src="img/hack.png">
 		</div>
-		
-		<div class="container">
+
+		<div id="hacktoday" class="container">
 			<div class="row">
 				<div class="col-md-5 col-sm-6 col-md-push-7 col-sm-push-6">
 					<h2>INI BUAT PENJELAS HACK TODAY</h2>
@@ -228,8 +285,8 @@
 		<div class="background-image-holder">
 			<img alt="image" class="background-image" src="img/app9.jpg">
 		</div>
-		
-		<div class="container">
+
+		<div id="businessit" class="container">
 			<div class="row">
 				<div class="col-md-5 col-sm-6 col-md-push-7 col-sm-push-6">
 					<h2>INI BUAT PENJELAS BUSINESS IT CASE</h2>
@@ -245,8 +302,8 @@
 		<div class="background-image-holder">
 			<img alt="image" class="background-image" src="img/app9.jpg">
 		</div>
-		
-		<div class="container">
+
+		<div id="semnasit" class="container">
 			<div class="row">
 				<div class="col-md-5 col-sm-6 col-md-push-7 col-sm-push-6">
 					<h2>INI BUAT PENJELAS NATIONAL SEMINAR</h2>
@@ -257,7 +314,7 @@
 			</div>
 		</div>
 	</section>
-	
+
 	</div>
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
@@ -270,4 +327,3 @@
     </body>
 </html>
 @endsection
-
