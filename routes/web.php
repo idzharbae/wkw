70f5432fe939baabcfdcff8f83d01fcf7bb47402
YuukiRen <reinaldoalvin1@gmail.com>
2018-06-02 05:22:11 +0700

    what

--- routes/web.php
+++ routes/web.php
@@ -43,13 +43,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 /***
 *ADMIN ROUTE, GAUSAH DIGANTI JADI PREFIX, tp klo mau, monggo. 
 ***/
-Route::get('admin/home','AdminController@main');
-Route::GET('admin','Admin\LoginController@showLoginForm')->name('admin.login');
-Route::POST('admin','Admin\LoginController@login');
-Route::GET('admin/main','AdminController@main');
-Route::POST('admin-password/email','Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
-Route::GET('admin-password/reset','Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-Route::POST('admin-password/reset','Admin\ResetPasswordController@reset');
-Route::GET('admin-password/reset/{token}','Admin\ResetPasswordController@showResetForm');
-Route::GET('admin/detail/{id}','AdminController@detail');
-Route::GET('admin/verify/{id}','AdminController@verify');
+Route::prefix('antiribetclub')->group(function(){
+	Route::get('/home','AdminController@main');
+	Route::get('/','Admin\LoginController@showLoginForm')->name('admin.login');
+	Route::post('/','Admin\LoginController@login');
+	Route::get('/main','AdminController@main');
+	Route::get('/detail/{id}','AdminController@detail');
+	Route::get('/verify/{id}','AdminController@verify');
+});
